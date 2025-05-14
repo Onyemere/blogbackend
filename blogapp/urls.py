2b7c68f4b56732bbe041b8c064e6f6bd082b2209
Victor <chukyrish@gmail.com>
2025-05-14 18:16:55 +0100

    url update

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('POST/user/', views.register_user, name='register_user'),
-    path('POST/blog/', views.create_blog, name='create_blog'),
-    path('GET/blog/', views.blog_list, name='blog_list'),
-    path('PUT/blog/<int:pk>/', views.update_blog, name='update_blog'),
-    path('DELETE/blog/<int:pk>/', views.delete_blog, name='delete_blog'),
-    path('PUT/user/', views.update_user_profile, name='update_user'),
+    path('user/', views.register_user, name='register_user'),
+    path('blog/', views.create_blog, name='create_blog'),
+    path('blog/', views.blog_list, name='blog_list'),
+    path('blog/<int:pk>/', views.update_blog, name='update_blog'),
+    path('blog/<int:pk>/', views.delete_blog, name='delete_blog'),
+    path('user/', views.update_user_profile, name='update_user'),
 ]
