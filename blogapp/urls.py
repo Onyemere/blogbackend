6314f64f2a1d2ae9276c435a3d9999468d349942
Victor <chukyrish@gmail.com>
2025-09-01 14:11:34 +0100

    yes

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('user/', views.register_user, name='register_user'),
-    path('blog/', views.create_blog, name='create_blog'),
-    path('blog/', views.blog_list, name='blog_list'),
-    path('blog/<int:pk>/', views.update_blog, name='update_blog'),
-    path('blog/<int:pk>/', views.delete_blog, name='delete_blog'),
-    path('user/', views.update_user_profile, name='update_user'),
+    path('register_user/', views.register_user, name='register_user'),
+    path('create_blog', views.create_blog, name='create_blog'),
+    path('blog_list/', views.blog_list, name='blog_list'),
+    path('update_blog/<int:pk>/', views.update_blog, name='update_blog'),
+    path('delete_blog/<int:pk>/', views.delete_blog, name='delete_blog'),
+    path('update_user/', views.update_user_profile, name='update_user'),
 ]
